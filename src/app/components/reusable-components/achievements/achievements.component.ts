39b666950027d13054cd2f87bb1e1649d7747038
SalmaHosny <salmahosny996@gmail.com>
2021-04-12 01:08:46 +0200

    header

--- src/app/components/reusable-components/achievements/achievements.component.ts
+++ src/app/components/reusable-components/achievements/achievements.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
-  styleUrls: ['./achievements.component.scss']
+  styleUrls: ['./achievements.component.scss'],
 })
 export class AchievementsComponent implements OnInit {
 
-  constructor() { }
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { HostListener } from '@angular/core';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 
-export class NavbarComponent implements OnInit {
+@HostListener('window:scroll', ['$event'])
 
-  constructor(private modalService: NgbModal, public authService: AuthService, public router: Router) { }
+export class NavbarComponent implements OnInit {
+  isCollapsed: boolean;
+  constructor(private modalService: NgbModal, public authService: AuthService, public router: Router) {
+    this.isCollapsed= true;
+  }
 
   ngOnInit(): void {
   }
@@ -29,6 +33,15 @@ export class NavbarComponent implements OnInit {
       this.router.navigate(['./client'])
     } else {
       this.router.navigate(['./mentor'])
+    }
+  }
+
+  onWindowScroll(e) {
+    let element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('bg-customColor');
+    } else {
+      element.classList.remove('bg-customColor');
     }
   }
 
